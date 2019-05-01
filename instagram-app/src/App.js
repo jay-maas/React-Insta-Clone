2cd80c39e57a574fa756b32432a464e6f66515b5
Jay Maas <jaymaas.dev@gmail.com>
2019-05-01 00:13:23 -0600

    Additional Stylings
    Working in some light touches to the sty lings for a better user experience.

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -2,6 +2,7 @@ import React from '../node_modules/react';
 import dummyData from './dummy-data';
 import CardContainer from './components/PostContainer/CardContainer';
 import SearchBar from './components/SearchBar/SearchBar';
+import Fuse from 'fuse.js';
 
 class App extends React.Component {
   constructor(props) {
@@ -13,16 +14,25 @@ class App extends React.Component {
 
   searchHandler = event => {
     event.preventDefault();
-    console.log(this.state.userData)
+    var options = {
+      shouldSort: true,
+      threshold: 0.6,
+      location: 0,
+      distance: 100,
+      maxPatternLength: 32,
+      minMatchCharLength: 1,
+      keys: [
+        "username"
+      ]
+    };
+    var fuse = new Fuse(this.state.userData, options); // "list" is the item array
+    var result = fuse.search(event.target.querySelector('.search').value);
     this.setState({
-      userData: this.state.userData.filter(user => {
-        return user.username === event.target.querySelector('.search').value
+      userData: result
       })
-    })
-    console.log(this.state.userData)
     event.target.reset();
   }
-
+  
   componentDidMount() {
     this.setState({
       userData: dummyData
